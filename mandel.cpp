ecb469cc1f0e99e7181d4cbf9297826e0287f050
iogameplayer <130794776+iogameplayer@users.noreply.github.com>
2023-08-08 20:53:26 +0200

    update yippie
    no epilepsy!
    one line at a time rendering for high iter counts
    optimizations

--- mandel.cpp
+++ mandel.cpp
@@ -8,27 +8,39 @@ double xPos=-2.0;
 double yPos=-1.0;
 double zoom=300;
 double startX,startY,start_X,start_Y;
-int maxIter=50;
+int maxIter =100;
+int maxIter2=100;
 double zoomFactor=1.0;
-double mouseX,mouseY; 
-float escapesmooth(double real, double imag){ // Optimized Escape Time Algorithm
+float period=0;
+bool render=true;
+float escapesmooth(double real, double imag){ // Optimized Escape Time Algorithm + smooth colors + perodicity checks
 	double r0=real;
 	double i0=imag;
 	float iter=0.0;
-	double tempreal;
+	double tempreal,io,ro;
 	while(iter<	maxIter&&real*real+imag*imag<=4){
 		tempreal=real*real-imag*imag+r0;
 		imag=2*real*imag+i0;
 		real=tempreal;
 		iter++;
+		if(real==ro&&imag==io){
+			iter=maxIter;
+			break;
+		}
+		period++;
+		if(period>20){
+			period=0;
+			ro=real;
+			io=imag;
+		}
 	}
 	if(iter<maxIter){
 		float log_zn=log((real*real)+(imag*imag))/2;
 		float nu=log(log_zn/log(2))/log(2);
 		iter=iter+1.0-nu;}
 	return iter;
-}
-/*int escape(double real, double imag){ // Escape time algorithm 
+}/*
+int escape(double real, double imag){ // Escape time algorithm 
 	double r0=real;
 	double ig0=imag;
 	int iter=0;
@@ -44,43 +56,56 @@ float escapesmooth(double real, double imag){ // Optimized Escape Time Algorithm
 
 void display(){
 	glClear(GL_COLOR_BUFFER_BIT);
-	for(int x=0;x<WIDTH;x++){
-		glBegin(GL_POINTS);
-		for(int y=0;y<HEIGHT;y++){
-			float iterations=escapesmooth(x/zoom+xPos,y/zoom+yPos);
-			glVertex2i(x,y);
-			if(iterations==maxIter){
-				glColor3f(0,0,0);
-			}
-			else{
-				float r1=0.5+sin(floor(iterations)/10)/2;
-				float r2=0.5+sin((floor(iterations)+1)/10)/2;
-				float g1=floor(iterations)/maxIter;
-				float g2=(floor(iterations)+1)/maxIter;
-				float b1=1-floor(iterations)/maxIter;
-				float b2=1-(floor(iterations)+1)/maxIter;
-				/*
-				float r1=floor(iterations)/32;
-				float r2=floor(iterations+1)/32;
-				float g1=floor(iterations)/64;
-				float g2=floor(iterations+1)/64;
-				float b1=floor(iterations)/128;
-				float b2=floor(iterations+1)/128;*/
-				
-				float t=fmod(iterations,1);
-                glColor3f(lerp(r1,r2,t),lerp(g1,g2,t),lerp(b1,b2,t));
-                //glColor3f(r1,g1,b1);
+	if(render){
+		for(int x=0;x<WIDTH;x++){
+			glBegin(GL_POINTS);
+			for(int y=0;y<HEIGHT;y++){
+				float iterations=escapesmooth(x/zoom+xPos,y/zoom+yPos);
+				glVertex2i(x,y);
+				if(iterations==maxIter){
+					glColor3f(0,0,0);
 				}
+				else{
+					float r1=0.5+sin(floor(iterations)/10)/2;
+					float r2=0.5+sin((floor(iterations)+1)/10)/2;
+					float g1=floor(iterations)/maxIter;
+					float g2=(floor(iterations)+1)/maxIter;
+					float b1=1-floor(iterations)/maxIter;
+					float b2=1-(floor(iterations)+1)/maxIter;
+					/*Sunburst:
+					float r1=floor(iterations)/32;
+					float r2=floor(iterations+1)/32;
+					float g1=floor(iterations)/64;
+					float g2=floor(iterations+1)/64;
+					float b1=floor(iterations)/128;
+					float b2=floor(iterations+1)/128;*/
+					/*test
+					float r1=iterations/32;
+					float r2=iterations/32;
+					float g1=iterations/64;
+					float g2=iterations/64;
+					float b1=iterations/128;
+					float b2=iterations/128;*/
+					
+					float t=fmod(iterations,1);
+					glColor3f(lerp(r1,r2,t),lerp(g1,g2,t),lerp(b1,b2,t));
+					//glColor3f(r1,g1,b1);
+					}
+			}
+			if(maxIter>100000){
+			glEnd();
+			glFlush();}
 		}
 		glEnd();
-		//glFlush();
+		glFlush();
+	if(maxIter2>1999999){
+		render=false;
+		}
 	}
-	glFlush();
-	glutSwapBuffers();
+	maxIter*=1.2;
+	if(render){glutSwapBuffers();}
 }
 void reshape(int w,int h){glutReshapeWindow(WIDTH,HEIGHT);}
-
-
 bool isDragging = false;
 int lastX,lastY;
 
@@ -92,11 +117,10 @@ void mouse(int button, int state, int x, int y){
         lastX=x;
         lastY=y;
     } else if(button==GLUT_LEFT_BUTTON&&state==GLUT_UP){
-        isDragging=false;}
+        isDragging=false;
+	}
 }
 void motion(int x, int y) {
-	mouseX = x;
-	mouseY = y;
     if (isDragging) {
         double deltaX=(x-lastX)/zoom;
         double deltaY=(y-lastY)/zoom;
@@ -104,17 +128,18 @@ void motion(int x, int y) {
         yPos+=deltaY;
         lastX=x;
         lastY=y;
+        maxIter=maxIter2;
         glutPostRedisplay();
     }
 }
 void key(unsigned char key,int x,int y){
 	switch(key){
-		case 'q':{zoom*=1.1;  break;}
-		case 'e':{zoom*=0.9;  break;}
-		case '=':{maxIter+=10;break;}
-		case '+':{maxIter+=10;break;}
-		case '-':{maxIter-=10;break;}
-		case 'r':{zoom=300;xPos=-2.0;yPos=-1.0;maxIter=50;break;}
+		case 'q':{zoom*=1.1;maxIter=maxIter2;render=true;break;}
+		case 'e':{zoom*=0.9;maxIter=maxIter2;render=true;break;}
+		case '=':{maxIter2+=1;maxIter=maxIter2;render=true;break;}
+		case '+':{maxIter2+=1;maxIter=maxIter2;render=true;break;}
+		case '-':{maxIter2-=1;maxIter=maxIter2;render=true;break;}
+		case 'r':{zoom=300;xPos=-2.0;yPos=-1.0;maxIter=50;render=true;break;}
 	}
 }
 
@@ -124,17 +149,17 @@ int main(int argc, char** argv){
 	glutInitWindowSize(WIDTH,HEIGHT);
 	glutCreateWindow("mandel badandel");
 	
-	glMatrixMode(GL_PROJECTION)
-	glLoadIdentity();
-	gluOrtho2D(0, WIDTH, 0, HEIGHT);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+	glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, WIDTH, 0, HEIGHT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 	glClearColor(0.0,0.0,0.0,1.0);
 	
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
-	glutMouseFunc(mouse);
-	glutMotionFunc(motion);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
 	glutKeyboardFunc(key);
 
 	glutMainLoop();
