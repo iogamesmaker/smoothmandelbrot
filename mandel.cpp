da24c912038b68ff4583d41d6b237bcd0c8f7b9a
iogamesplayer <130794776+iogameplayer@users.noreply.github.com>
2023-09-12 18:13:02 +0200

    Update mandel.cpp

--- mandel.cpp
+++ mandel.cpp
@@ -2,27 +2,82 @@
 #include <iostream>
 #include <cmath>
 float lerp(float a,float b,float t){return a+t*(b-a);}
-const int WIDTH =800;
-const int HEIGHT=600;
-float r1,r2,g1,g2,b1,b2;
+int WIDTH =800;
+int HEIGHT=600;
+int mouseX,mouseY;
 bool biomorph=false;
-double xPos=-3.2;
-double yPos=-2.0;
-double zoom=150;
-double startX,startY,start_X,start_Y;
-int maxIter =50;
-int maxIter2=50;
-double zoomFactor=1.0;
-float period=0;
+bool julia=false;
+bool smooth=false;
 bool render=true;
-float escapesmooth(double real, double imag){ // Optimized Escape Time Algorithm + smooth colors + perodicity checks
-	double r0=real;
-	double i0=imag;
-	float iter=0.0;
-	double tempreal,io,ro;
+long double xPos=-3.2;
+long double yPos=-2.0;
+long double zoom=150;
+long double xJul,yJul;
+int fractal=1;
+long double startX,startY,start_X,start_Y;
+int maxIter =1000;
+int maxIter2=1000;
+long double zoomFactor=1.0;
+float period=0,iterations=0,iter=0,log_zn,nu,r1,r2,g1,g2,b1,b2,deltaX,deltaY,t;
+void reset(){
+	xPos=-3.2;yPos=-2.0;zoom=150;render=true;glutPostRedisplay();
+}
+void key(unsigned char key, int x, int y) {
+	switch (key){
+		case 'q': {
+			zoom *= 1.1;
+			maxIter = maxIter2;
+			render = true;
+			glutPostRedisplay();
+			break;
+		}
+
+		case 'e': {
+			zoom *= 0.9;
+			maxIter = maxIter2;
+			render = true;
+			glutPostRedisplay();
+			break;
+		}
+        case '+': {maxIter2 *= 1.1;maxIter = maxIter2;render = true;glutPostRedisplay();break;}
+        case '=': {maxIter2 *= 1.1;maxIter = maxIter2;render = true;glutPostRedisplay();break;}
+        case '-': {maxIter2 /= 1.1;maxIter = maxIter2;render = true;glutPostRedisplay();break;}
+		case 'b': {biomorph=!biomorph;glutPostRedisplay();break;}
+		case 's': {smooth  =!  smooth;glutPostRedisplay();break;}
+		case '2': {fractal=2;std::cout << "-Burning Ship-"; break;}
+		case '1': {fractal=1;std::cout << "Mandelbrot Set";break;}
+        case 'r': {reset();break;}
+        case 'j': {
+			if(julia){
+				julia=false;
+				reset();
+				std::cout << "-Fractal Mode-\n";
+			}
+			else{
+				julia=true;
+				xJul=mouseX/zoom+xPos;
+				yJul=mouseY/zoom+yPos;
+				reset();
+				std::cout << "--Julia Mode--\nReal:" << xJul << "\nImag:" << yJul << "\n--------------\n";
+			}
+			break;
+		}
+	}
+}
+long double r0,i0,real,imag,tempreal,io,ro;
+float escapesmooth(long double real, long double imag, int x, int y){
+		r0=real;
+		i0=imag;
+		if(julia){
+			r0=xJul;i0=yJul;
+		}
+	iter=0.0;
 	while(iter<maxIter&&real*real+imag*imag<=16){
 		tempreal=real*real-imag*imag+r0;
-		imag=2*real*imag+i0;
+		if(fractal==2){
+		imag=-2*abs(real*imag)+i0;}
+		else if(fractal==1){
+		imag=2*real*imag+i0;}
 		real=tempreal;
 		iter++;
 		if(real==ro&&imag==io){
@@ -31,7 +86,7 @@ float escapesmooth(double real, double imag){ // Optimized Escape Time Algorithm
 		}
 		//interesting effect
 		if(biomorph){
-			if(abs(real)>0.75&&abs(imag)>0.75){
+			if(abs(real)>4&&abs(imag)>4){
 				iter=real+imag;
 				break;
 			}
@@ -43,63 +98,65 @@ float escapesmooth(double real, double imag){ // Optimized Escape Time Algorithm
 			io=imag;
 		}
 	}
-	if(iter<maxIter){
-		float log_zn=log((real*real)+(imag*imag))/2;
-		float nu=log(log_zn/log(2))/log(2);
+	if(smooth&&iter<maxIter){
+		log_zn=log((real*real)+(imag*imag))/2;
+		nu=log(log_zn/log(2))/log(2);
 		iter=iter+1.0-nu;
 	}
 	return iter;
 }
-
 void display(){
-	if(render){
-		for(int x=0;x<WIDTH;x++){
-			glBegin(GL_POINTS);
-			for(int y=0;y<HEIGHT;y++){
-				float iterations=escapesmooth(x/zoom+xPos,y/zoom+yPos);
-				glVertex2i(x,y);
-				if(iterations==maxIter){
-					glColor3f(0,0,0);
-				}
-				else{
-					float r1=0.5+sin(floor(iterations)/10)/2;
-					float g1=floor(iterations)/maxIter;
-					float b1=1-floor(iterations)/maxIter;
+		if(render){
+			for(int y=HEIGHT;y>0;y--){
+				glBegin(GL_POINTS);
+				for(int x=0;x<WIDTH;x++){
+					iterations=escapesmooth(x/zoom+xPos,y/zoom+yPos,xJul,yJul);
+						glVertex2i(x,y);
+					if(iterations==maxIter){
+						glColor3f(0,0,0);
+					}
+					else{
+						r1=0.5+sin(floor(iterations)/10)/2;
+						g1=floor(iterations)/maxIter;
+						b1=1-floor(iterations)/maxIter;
+						if(smooth){
+							r2=0.5+sin((floor(iterations)+1)/10)/2;
+							g2=  (floor(iterations)+1)/maxIter;
+							b2=1-(floor(iterations)+1)/maxIter;}
+						/*Sunburst:*//*
+						float r1=floor(iterations)/32;
+						float r2=floor(iterations+1)/32;
+						float g1=floor(iterations)/64;
+						float g2=floor(iterations+1)/64;
+						float b1=floor(iterations)/128;
+						float b2=floor(iterations+1)/128;
+						/*test*//*
+						float r1=floor(iterations)/32;
+						float r2=floor(iterations+1)/32;
+						float g1=floor(iterations)/64;
+						float g2=floor(iterations+1)/64;
+						float b1=floor(iterations)/128;
+						float b2=floor(iterations+1)/128;*/
 
-					float r2=0.5+sin((floor(iterations)+1)/10)/2;
-					float g2=  (floor(iterations)+1)/maxIter;
-					float b2=1-(floor(iterations)+1)/maxIter;
-					/*Sunburst:*//*
-					float r1=floor(iterations)/32;
-					float r2=floor(iterations+1)/32;
-					float g1=floor(iterations)/64;
-					float g2=floor(iterations+1)/64;
-					float b1=floor(iterations)/128;
-					float b2=floor(iterations+1)/128;
-					/*test*//*
-					float r1=floor(iterations)/32;
-					float r2=floor(iterations+1)/32;
-					float g1=floor(iterations)/64;
-					float g2=floor(iterations+1)/64;
-					float b1=floor(iterations)/128;
-					float b2=floor(iterations+1)/128;*/
-
-					float t=fmod(iterations,1);
-					glColor3f(lerp(r1,r2,t),lerp(g1,g2,t),lerp(b1,b2,t));
+						t=fmod(iterations,1);
+						if(smooth){
+							if(x==1&&y==1){std::cout << "a";}
+							glColor3f(lerp(r1,r2,t),lerp(g1,g2,t),lerp(b1,b2,t));}
+						else{
+							glColor3f(r1,g1,b1);
+							}
 				}
 			}
-			glEnd();
-			glFlush();}
-		}
+	glEnd();
+	glFlush();}
+	}
 	//maxIter*=1.2;
 }
-void reshape(int w,int h){glutReshapeWindow(WIDTH,HEIGHT);}
+void reshape(int w,int h){glutReshapeWindow(w,h);HEIGHT=h;WIDTH=w;}
 bool isDragging = false;
 int lastX,lastY;
 
 void mouse(int button, int state, int x, int y){
-    double mouseX=static_cast<double>(x)/WIDTH;
-    double mouseY=static_cast<double>(y)/HEIGHT;
     if(button==GLUT_LEFT_BUTTON&&state==GLUT_DOWN){
         isDragging=true;
         lastX=x;
@@ -110,8 +167,8 @@ void mouse(int button, int state, int x, int y){
 }
 void motion(int x, int y) {
     if (isDragging) {
-        double deltaX=(x-lastX)/zoom;
-        double deltaY=(y-lastY)/zoom;
+        deltaX=(x-lastX)/zoom;
+        deltaY=(y-lastY)/zoom;
         xPos-=deltaX;
         yPos+=deltaY;
         lastX=x;
@@ -120,31 +177,11 @@ void motion(int x, int y) {
         glutPostRedisplay();
     }
 }
-void key(unsigned char key, int x, int y) {
-	switch (key){
-    case 'q': {
-        zoom *= 1.1;
-        maxIter = maxIter2;
-        render = true;
-		glutPostRedisplay();
-        break;
-    }
 
-    case 'e': {
-        zoom *= 0.9;
-        maxIter = maxIter2;
-        render = true;
-		glutPostRedisplay();
-        break;
-    }
-        case '=': {maxIter2 += 1;maxIter = maxIter2;render = true;glutPostRedisplay();break;}
-        case '+': {maxIter2 += 1;maxIter = maxIter2;render = true;glutPostRedisplay();break;}
-        case '-': {maxIter2 -= 1;maxIter = maxIter2;render = true;glutPostRedisplay();break;}
-		case 'b': {if(biomorph){biomorph=false;}else{biomorph=true;}glutPostRedisplay();break;}
-        case 'r': {double xPos=-3.2;double yPos=-2.0;double zoom=150;maxIter = 50;render = true;glutPostRedisplay();break;}
-	}
+void onMouseMove(int x, int y) {
+    mouseX = x;
+    mouseY = y;
 }
-
 int main(int argc, char** argv){
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
@@ -152,16 +189,17 @@ int main(int argc, char** argv){
 	glutCreateWindow("mandel badandel");
 
 	glMatrixMode(GL_PROJECTION);
-    	glLoadIdentity();
-	gluOrtho2D(0, WIDTH, 0, HEIGHT);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+    glLoadIdentity();
+    gluOrtho2D(0, WIDTH, 0, HEIGHT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 	glClearColor(0.0,0.0,0.0,1.0);
 
+	glutPassiveMotionFunc(onMouseMove);
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
-	glutMouseFunc(mouse);
-	glutMotionFunc(motion);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
 	glutKeyboardFunc(key);
 
 	glutMainLoop();
